cafd3f11d0630b424510481045e1c02fd71f0b63
deim76 <53413337+deim76@users.noreply.github.com>
2019-09-18 16:48:33 +1000

    Update task3.sql

--- task3.sql
+++ task3.sql
@@ -1,9 +1,9 @@
 # Создание тестовой DATABASE
-create database test;
-use test;
+CREATE DATABASE test;
+USE test;
 
 # Создание Таблицы country_tab
-create table country_tab (id INT UNSIGNED NOT NULL AUTO_INCREMENT, code char(3) NOT NULL unique,
+CREATE TABLE country_tab (id INT UNSIGNED NOT NULL AUTO_INCREMENT, code char(3) NOT NULL unique,
   name char(52) NOT NULL unique,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (id)); 
@@ -11,31 +11,31 @@ create table country_tab (id INT UNSIGNED NOT NULL AUTO_INCREMENT, code char(3) 
   # INSERT
   INSERT INTO country_tab (name , code) value('China', 'ch');
   INSERT INTO country_tab  value(default, 'kh','Korea', DEFAULT);
-  insert into country_tab SET name='Ispania', code='is';
-  insert into country_tab (name, code) value ('France','fr'),('Italy','it'),('Greek','gr');
-  insert IGNORE into country_tab SET name='Ispania', code='is';
+  INSERT INTO country_tab SET name='Ispania', code='is';
+  INSERT INTO country_tab (name, code) value ('France','fr'),('Italy','it'),('Greek','gr');
+  INSERT IGNORE INTO country_tab SET name='Ispania', code='is';
  
 # для заполнения используем example base MYSQL world Table country 
 # первые 10 строк
-insert IGNORE INTO  country_tab (code,name) 
-	select code,name  from world.country Limit 10; 
+INSERT IGNORE INTO  country_tab (code,name) 
+	SELECT code,name  from world.country Limit 10; 
 
 
 #SELECT
-select * from  country_tab;
-select id, CONCAT(name, code) as name_code from  country_tab order by name DESC limit 4;
-select * from  country_tab order by name DESC limit 4;
-select name , DAY(created_at) from country_tab order by name DESC limit 4;
-select distinct DAY(created_at) from country_tab order by name DESC limit 4;
-select name from country_tab order by name DESC   limit 4 OFFSET 2;
+SELECT * FROM  country_tab;
+SELECT id, CONCAT(name, code) as name_code FROM  country_tab ORDER BY name DESC LIMIT 4;
+SELECT * FROM  country_tab ORDER BY name DESC limit 4;
+SELECT name , DAY(created_at) FROM country_tab ORDER BY name DESC LIMIT 4;
+SELECT distinct DAY(created_at) FROM country_tab ORDER BY name DESC LIMIT 4;
+SELECT name FROM country_tab ORDER BY name DESC   LIMIT 4 OFFSET 2;
  
  
   # REPLACE
   # поля name и code установлены unique
  REPLACE INTO country_tab (name , code) value('China', 'ssh');
  REPLACE INTO country_tab  value(default, 'khh','Korea', DEFAULT);
- REPLACE into country_tab SET name='Ispaniola', code='is';
- REPLACE into country_tab (name, code) value ('Franc','fr'),('Ital','it'),('Greek','gr'); 
+ REPLACE INTO country_tab SET name='Ispaniola', code='is';
+ REPLACE INTO country_tab (name, code) value ('Franc','fr'),('Ital','it'),('Greek','gr'); 
 
   
 
@@ -55,7 +55,7 @@ DELETE FROM country_tab ORDER BY name DESC LIMIT 5 ;
 # TRUNCATE
 TRUNCATE country_tab;
 
-Select * from country_tab;  
+SELECT * FROM country_tab;  
 
 
 
